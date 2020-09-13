f76eb7df93faa282648644cb8d2ca5105c3fc347
yoavsadeh <yoav.sadeh@saga.org>
2020-09-13 15:47:39 +0300

    rename + todo

--- word_frequencies/app.py
+++ word_frequencies/app.py
@@ -6,10 +6,12 @@ from word_frequencies.data.concrete.sqlite_data_store import SQLiteDataStore
 from word_frequencies.data.contract.word_frequency_store import WordFrequencyStore
 from word_frequencies.word_frequency_services import extract_word_frequencies, extract_words, download_text
 import logging
+
+# TODO: logger config doesn't apply - need to fix
 from logging.config import fileConfig
 
-log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.conf')
-fileConfig(log_file_path)
+log_setting_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.conf')
+fileConfig(log_setting_file_path)
 
 logger = logging.getLogger(__name__)
 
